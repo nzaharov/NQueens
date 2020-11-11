4559f7312e27f29d78ccbcc5e5a3612d9278b2e7
Nikolay Zaharov <nzaharov@x570.localdomain>
2020-11-11 23:49:11 +0200

    init 2

--- src/main.rs
+++ src/main.rs
@@ -13,20 +13,10 @@ fn main() -> io::Result<()> {
 
     let n = buffer.trim_end().parse::<usize>().expect("Invalid input ");
 
-    // let mut board: Vec<Vec<u8>> = (0..n).map(|_| vec![0; n]).collect();
     let mut board = Board::new(n);
     let mut queens: Vec<Queen> = Vec::with_capacity(n);
 
     for i in 0..n {
-        // let mut q: Queen;
-
-        // while {
-        //     q = Queen {
-        //         x: rng.gen_range(0, n),
-        //         y: rng.gen_range(0, n),
-        //     };
-        //     board.is_taken(q.x, q.y)
-        // } {}
         let q = Queen {
             x: i,
             y: rng.gen_range(0, n),
@@ -36,20 +26,6 @@ fn main() -> io::Result<()> {
 
         queens.push(q);
     }
-
-    // while true {}
-
-    // let output = board.into_iter().fold(String::new(), |acc, row| {
-    //     let line = row
-    //         .iter()
-    //         .map(|v| match v {
-    //             1 => "*",
-    //             _ => "_",
-    //         })
-    //         .collect::<Vec<&str>>()
-    //         .join(" ");
-    //     acc + &line + "\n"
-    // });
 
     println!("{}", board);
 
